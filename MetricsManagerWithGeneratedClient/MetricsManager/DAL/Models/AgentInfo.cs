25c5460b03a2479e3ae5e0bda50e29d94bf351fc
Alexander <alexander_freeman85@mail.ru>
2021-05-30 22:53:46 +0300

    Lesson-8 added

---
+++ MetricsManagerWithGeneratedClient/MetricsManager/DAL/Models/AgentInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MetricsManager.Models
+{
+    public class AgentInfo
+    {
+        public int AgentId { get; set; }
+        public string AgentAddress { get; set; }
+        public int Status { get; set; }
+    }
+}
